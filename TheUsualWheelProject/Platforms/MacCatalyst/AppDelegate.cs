d1539040e75175e02051494b0c1fe14b4b73e4ba
EyeMustache <lucioridder2003@gmail.com>
2026-03-12 04:19:09 +0100

    Generated Maui and created folder structure
    Ran the maui install and added the base folders

---
+++ TheUsualWheelProject/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TheUsualWheelProject;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
