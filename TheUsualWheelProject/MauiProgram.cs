ef41a1cc2fdebb724026be4d991f46482e2ad569
EyeMustache <lucioridder2003@gmail.com>
2026-03-12 04:27:19 +0100

    Update MauiProgram.cs
    Added SkiaSharp

--- TheUsualWheelProject/MauiProgram.cs
+++ TheUsualWheelProject/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using SkiaSharp.Views.Maui.Controls.Hosting;
 
 namespace TheUsualWheelProject;
 
@@ -14,6 +15,9 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
+		
+		builder.UseSkiaSharp();
+
 
 #if DEBUG
 		builder.Logging.AddDebug();
